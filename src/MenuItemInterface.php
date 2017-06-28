a80f02787a2c384f8373b5694316ba7a49a12c9b
Miguel Muscat <miguelmuscat93@gmail.com>
2017-06-28 12:20:09 +0200

    Added `MenuItemInterface`

---
+++ src/MenuItemInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RebelCode\WordPress\Admin\Menu;
+
+/**
+ * Something that represents an item in a menu.
+ *
+ * @since [*next-version*]
+ */
+interface MenuItemInterface extends MenuElementInterface
+{
+}
